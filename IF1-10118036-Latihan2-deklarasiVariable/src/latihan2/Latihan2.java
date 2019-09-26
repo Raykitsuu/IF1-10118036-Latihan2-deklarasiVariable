71b7b01d61799ae19f9bceac6a08b73af90c5af4
Adrian C. Senna <55816324+Raykitsuu@users.noreply.github.com>
2019-09-26 19:26:40 +0700

    Add files via upload

---
+++ IF1-10118036-Latihan2-deklarasiVariable/src/latihan2/Latihan2.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan2;
+
+/**
+ *
+ * @author
+ * NAMA			: Adrian C. Senna
+ * KELAS		: IF-1
+ * NIM			: 10118036
+ * Deskripsi Program	: Program ini berisi program untuk mendeklrasikan variable yang telah ditentukan.
+ *
+ **/
+public class Latihan2 {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		// TODO code application logic here
+		int nilaiInt;
+		final double PHI = 3.14;
+		boolean nilaiLogika;
+		char nilaiKarakter;
+
+		//nilai variable
+		nilaiInt = 78;
+		nilaiLogika = false;
+		nilaiKarakter = 'D';
+
+		//menampilkan hasil
+
+		System.out.println();
+		System.out.println("Isi variable nilai = " + nilaiInt);
+		System.out.println("Isi variable PHI = " + PHI);
+		System.out.println("Isi variable logika = " + nilaiLogika);
+		System.out.println("Isi variable Karakter = " + nilaiKarakter);
+	}
+	
+}
